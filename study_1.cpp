ece644357508d781f41bdff95ea954c74b47b4d5
Tmxla <jgo030256@gmail.com>
2025-09-03 16:36:41 +0900

    commit_2 simple_practice

--- study_1.cpp
+++ study_1.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "Hello, World!/n" << endl;
+
+    string name;
+    int age = 0;
+    cout << "이름과 나이를 입력하시오: ";
+    cin >> name >> age;
+
+    if (age < 19) {
+        cout << "당신은 미자\n";
+        cout << name;
+    }
+    else {
+        cout << "당신은 성인\n";
+        cout << name;
+    }
+
     return 0;
 }
